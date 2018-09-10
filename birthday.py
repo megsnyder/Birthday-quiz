b312da7dcfff7cc6422bc3e4ffb187d6c327ad8a
megsnyder <42872006+megsnyder@users.noreply.github.com>
2018-09-10 12:10:54 -0400

    Updated from Brython Server: 9/10/2018 12:10:54 PM

--- birthday.py
+++ birthday.py
@@ -38,6 +38,9 @@ todaydate = datetime.today().day
 month = month_name[todaymonth]
 
 name = input("Hello, what is your name? ")
-month = input("Hi " + name + ", what was the name of the month you were born in? ")
+mnth = input("Hi " + name + ", what was the name of the month you were born in? ")
 year = input("And what year were you born in, " + name + "? ")
 day = input("And the day? ")
+mnth = datetime.today().month
+if mnth == :
+    print("You were born on Halloween!")
