a3c81335836659396024639d8e6e5b34e6440dc3
megsnyder <42872006+megsnyder@users.noreply.github.com>
2018-09-10 12:05:18 -0400

    Updated from Brython Server: 9/10/2018 12:05:17 PM

--- birthday.py
+++ birthday.py
@@ -38,3 +38,5 @@ todaydate = datetime.today().day
 month = month_name[todaymonth]
 
 name = input("Hello, what is your name? ")
+month = input("Hi " + name + ", what was the name of the month you were born in? ")
+year = input("And what year were you born in, " + name + "? ")
